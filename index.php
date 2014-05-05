088ae0429df8fa0b3e2b09302b6ccea652ad8b27
dr-spangle <dr-spangle@dr-spangle.com>
2014-05-05 01:44:41 +0100

    added blocks of text for intro, hallo, about me and programming

--- index.php
+++ index.php
@@ -70,13 +70,23 @@ $getstr = substr($getstr,0,-1);
 </div>
 
 <div id="content" >
-Content content<br />
+This website is simply about myself and the projects I embark upon. Most don’t go as planned, but hopefully I learn from them all, no matter the results. Here shall hopefully be posts and notes on how that learning goes.<br />
+
 <h2><a href="Hallo">Hallo</a></h2>
-The hot water from the Martian's overthrow drifted downstream with me, so that for the best part of a mile I could see little of either bank. Once, however, I made out a string of black figures hurrying across the meadows from the direction of Weybridge. Halliford, it seemed, was deserted, and several of the houses facing the river were on fire. It was strange to see the place quite tranquil, quite desolate under the hot blue sky, with the smoke and little threads of flame going straight up into the heat of the afternoon. Never before had I seen houses burning without the accompaniment of an obstructive crowd. A little farther on the dry reeds up the bank were smoking and glowing, and a line of fire inland was marching steadily across a late field of hay.
+Hallo is my main project at the moment. He’s an IRC bot with far too many features and more bugs than I would like. Eventually I’ll get it all ironed out, but I’m still working towards that goal.<br />
+Currently hallo has some features to manage channels (including kick, voice, op, etc) along with many features useful to users (choosing options from given lists, calculating and converting values, and reading the titles from posted URLs).<br />
+He also has many functions which are apparently useful to someone, somewhere, such as choosing a random <a href=“http://bulbapedia.bulbagarden.net”>Pokemon</a> or a random <a href=“http://equestriadaily.com”>pony</a>.<br />
+I seem to have a constantly long list of improvements and additions to apply to hallo, so there should be some quantity of posts in that category.<br />
+
 <h2><a href="About_me">About me</a></h2>
-I <a href="continue">continue</a> my existance.
+Some overview of myself, my life, and the things around me. Along with occasional posts on updates to those things and perhaps some insights into how I think and work. Some posts about other things I’m interested in or involved in will most probably appear here.<br />
+As a rather brief intro to this section, I’m a guy living in Swansea, Wales. I originally grew up in a small town in Northamptonshire and I work as a programmer for a local company operating nation-wide. I originally came down to Swansea for university to study mathematics, but that didn’t turn out as it was originally planned to. But thanks to some programming experience I picked up over the years, I managed to get a job in the area.<br />
+
 <h2><a href="Programming">Programming</a></h2>
-Then suddenly we saw a rush of smoke far away up the river, a puff of smoke that jerked up into the air and hung; and forthwith the ground heaved under foot and a heavy explosion shook the air, smashing two or three windows in the houses near, and leaving us astonished.
+The first programming I did was a strange attempt to make tools and applications in some software called <a href=“http://en.wikipedia.org/wiki/GameMaker:_Studio”>GameMaker</a> I started with their drag and drop interface, but rapidly moved to using their custom GameMaker Language (GML), which is apparently syntactically close to C++, although I’ve forgotten anything about GML by now.<br />
+I quickly moved on to PHP. At first just using it to include headers and footers in multiple pages, but quickly trying to build resource trading and economics simulators for a hobby I was involved in at the time. I later moved to building some pretty powerful systems for measuring forum activity in the community I was involved in, which developed my knowledge of SQL, largely using MySQL.<br />
+My knowledge of programming mostly stagnated on PHP for a few years after this, using the language to do tasks much better suited to other languages. It wasn’t until some years later I started developing Hallo and therefore learning Python. I expanded my knowledge pretty rapidly as I worked through <a href=“https://projecteuler.net/“>Project Euler</a> problems and adding miscellaneous functions to Hallo. My knowledge expanded more rapidly since getting employed as a Software Engineer, accelerating my knowledge of Python and starting my path to learning more languages, with sights set on Java, C# and C++.<br />
+
 <h2><a href="Sewing">Sewing</a></h2>
 <p>At this moment, also, a long black funnel, crowned with wreaths of smoke, appeared on the edge of the waters.  It was the American steamer, leaving for Yokohama at the appointed time.</p>
 <p>"Confound her!" cried John Bunsby, pushing back the rudder with a desperate jerk.</p>
